3e32394725ba0408e332112336231b93baf667c0
Chris Abad <chrisabad@gmail.com>
2015-08-27 10:02:59 -0700

    Ported over Steven's changes

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -19,3 +19,29 @@ var data = [
 
 var ctx = document.getElementById('progress').getContext('2d');
 var progressChart = new Chart(ctx).Doughnut(data, { segmentShowStroke: false });
+
+// setInterval(function () {
+//     setRandomClass();
+//     randomWait = Math.floor((Math.random() * 20000) + 2000);
+// }, 2000);
+
+function setRandomClass(items) {
+   var number = items.length;
+   var random = Math.floor((Math.random() * number));
+   items.eq(random).addClass("pulse");
+   setTimeout(function() {
+       items.eq(random).removeClass("pulse");
+   }, 2000);
+}
+
+(function loop() {
+   var rand = Math.round(Math.random() * (8000 - 200)) + 200;
+
+   var ul = $(".section--main .list");
+   var items = ul.find(".tick");
+
+   setTimeout(function() {
+       setRandomClass(items);
+       loop();
+   }, rand);
+}());
